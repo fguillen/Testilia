a42d7da0250cd92d3b01fec3cec97a8e8a6987a2
Fernando Guillen <fguillen.mail@gmail.com>
2010-09-26 20:07:05 +0200

    cleaning

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,10 @@ num_of_example_examns = 20 # change this number if you want more
 cvs_files = "#{RAILS_ROOT}/etc/escuela_balear_tests/cvs/*.cvs"
 indexes = File.read( "#{RAILS_ROOT}/etc/escuela_balear_tests/tests_index_per.txt" )
 
+puts "EXAMPLE EXAMNS"
+puts "--------------"
 puts "cvs_files: #{cvs_files}"
+puts "creating #{num_of_example_examns} example examns"
 
 Dir.glob( cvs_files )[0..20].each do |cvs_path|
   puts ">> processing: #{cvs_path}"
@@ -25,11 +28,13 @@ Dir.glob( cvs_files )[0..20].each do |cvs_path|
   exam = Exam.create_from_cvs( exam_name, cvs_path )
   exam.update_attributes( :kind => exam_kind, :position => exam_id )
   
-  puts "<< Exam created: '#{exam.kind} | #{exam.name}'"
+  puts "<< Exam created: '#{exam.kind} | #{exam.name}'\n"
 end
 
 user = Factory(:user, :email => 'email@email.com', :password => 'pass', :password_confirmation => 'pass' )
 
+puts "\nEXAMPLE USER:"
+puts "--------------"
 puts "User created:"
 puts "Email: email@emai.com"
 puts "Password: pass"
